b34308dca670cfe9a09dee410d67774ce4077e82
samirls <samirlaguardiaii@gmail.com>
2023-12-28 16:14:28 -0300

    full button response

--- app/contact/Card.js
+++ app/contact/Card.js
@@ -2,33 +2,37 @@ import { Box, Button } from "@chakra-ui/react";
 import Link from "next/link";
 import React from "react";
 
-function Card({icon, title, subTitle, buttonName, href}) {
-
-
+function Card({ icon, title, subTitle, buttonName, href }) {
   return (
     <Box
-      w={{base: '80%', lg:"50%"}}
-      h={{base: '150px', lg:"15vh"}}
+      w={{ base: "80%", lg: "50%" }}
+      h={{ base: "150px", lg: "15vh" }}
       bg="linear-gradient(to top, #e0e7ea8a 0%, #e0e7ea36 100%);"
       borderRadius="30px"
       display="flex"
       flexDir="column"
       justifyContent="center"
       alignItems="center"
-      mb={{base: '0', lg:"15px"}}
+      mb={{ base: "0", lg: "15px" }}
       boxShadow="md"
       _hover={{
         boxShadow: "lg",
       }}
       transition="500ms"
     >
-      <Box fontSize="1.6rem">
-        {icon}
+      <Box fontSize="1.6rem">{icon}</Box>
+      <Box pt="10px" fontWeight="800">
+        {title}
       </Box>
-      <Box pt="10px"  fontWeight="800">{title}</Box>
-      <Box mt="-5px" fontWeight="400">{subTitle}</Box>
+      <Box mt="-5px" fontWeight="400">
+        {subTitle}
+      </Box>
       <Box pt="10px">
-        <Button size='sm' colorScheme='purple' color='white'><Link href={href} target="_blank">{buttonName}</Link></Button>
+        <Link href={href} target="_blank">
+          <Button size="sm" colorScheme="purple" color="white">
+            {buttonName}
+          </Button>
+        </Link>
       </Box>
     </Box>
   );
