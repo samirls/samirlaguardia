ab52cbb05717efc9f625892fec3fff10f590c0b2
samirls <samirlaguardiaii@gmail.com>
2024-02-14 15:08:39 -0300

    added hover effect to navitem

--- app/ui/sidenav/NavItem.js
+++ app/ui/sidenav/NavItem.js
@@ -5,13 +5,18 @@ export default function NavItem({ icon, label, path, isSelected }) {
   return (
     <Link href={path}>
       <Box
-        display="flex"
-        alignItems="center"
-        bg={isSelected ? "blue.100" : ""}
-        _hover={{ bg: "blue.100" }}
-        borderRadius="9px"
-        height="50px"
-        cursor="pointer"
+       display="flex"
+       alignItems="center"
+       bg={isSelected ? "blue.100" : ""}
+       borderRadius="9px"
+       height="50px"
+       cursor="pointer"
+       transition="transform 0.3s ease, box-shadow 0.3s ease, background-color 0.3s ease"
+       _hover={{
+         transform: "scale(1.05)",
+         boxShadow: "0 3px 6px rgba(0, 0, 0, 0.2)",
+         backgroundColor: isSelected ? "blue.200" : "blue.50",
+       }}
       >
         <Box fontSize="1.3rem" display="flex" alignItems="center">
           <Box pr="12px">{icon}</Box>
