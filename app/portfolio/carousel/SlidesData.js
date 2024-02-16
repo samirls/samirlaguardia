b570cfbe85650a28a4a674b8a8bf741ac74464fa
samirls <samirlaguardiaii@gmail.com>
2024-02-16 01:11:50 -0300

    cleaner code to carousel

---
+++ app/portfolio/carousel/SlidesData.js
@@ -0,0 +1,115 @@
+import { FaJava, FaReact, FaHtml5, FaCss3Alt } from "react-icons/fa";
+import {
+  BiLogoTypescript,
+  BiLogoSpringBoot,
+  BiLogoPostgresql,
+} from "react-icons/bi";
+import { TbBrandNextjs } from "react-icons/tb";
+import { SiMongodb, SiExpress, SiMongoose, SiJavascript } from "react-icons/si";
+
+export const SlidesData = [
+  {
+    src: "/img/task-list-app.png",
+    liveLink: "https://todo-next-14-kappa.vercel.app/",
+    githubLink: "https://github.com/samirls/todo-next-14",
+    skills: [
+      { icon: <TbBrandNextjs />, color: "gray.800" },
+      { icon: <BiLogoTypescript />, color: "blue.500" },
+      { icon: <BiLogoPostgresql />, color: "blue.700" },
+    ],
+  },
+  {
+    src: "/img/drake-equation.png",
+    liveLink: "https://drake-equation-next-14.vercel.app/",
+    githubLink: "https://github.com/samirls/drake-equation-next-14",
+    skills: [
+      { icon: <TbBrandNextjs />, color: "gray.800" },
+      { icon: <BiLogoTypescript />, color: "blue.500" },
+      { icon: <BiLogoPostgresql />, color: "blue.700" },
+    ],
+  },
+  {
+    src: "/img/todo-family.png",
+    liveLink: "notDeployed",
+    githubLink: "https://github.com/samirls/todo-family-api",
+    skills: [
+      { icon: <FaReact />, color: "blue.600" },
+      { icon: <FaJava />, color: "orange.700" },
+      { icon: <BiLogoSpringBoot />, color: "green.400" },
+      { icon: <BiLogoPostgresql />, color: "blue.700" },
+    ],
+  },
+  {
+    src: "/img/minha-clinica.png",
+    liveLink: "notDeployed",
+    githubLink: "https://github.com/samirls/med-track-api",
+    skills: [
+      { icon: <TbBrandNextjs />, color: "gray.800" },
+      { icon: <FaJava />, color: "orange.700" },
+      { icon: <BiLogoSpringBoot />, color: "green.400" },
+      { icon: <BiLogoPostgresql />, color: "blue.700" },
+    ],
+  },
+  {
+    src: "/img/workout-buddy.png",
+    liveLink: "notDeployed",
+    githubLink: "https://github.com/samirls/workout-buddy-be",
+    skills: [
+      { icon: <FaReact />, color: "blue.600" },
+      { icon: <SiExpress />, color: "black" },
+      { icon: <SiMongoose />, color: "red.700" },
+      { icon: <SiMongodb />, color: "green.600" },
+    ],
+  },
+  {
+    src: "/img/workout-buddy.png",
+    liveLink: "notDeployed",
+    githubLink: "https://github.com/samirls/workout-buddy-be",
+    skills: [
+      { icon: <FaReact />, color: "blue.600" },
+      { icon: <SiExpress />, color: "black" },
+      { icon: <SiMongoose />, color: "red.700" },
+      { icon: <SiMongodb />, color: "green.600" },
+    ],
+  },
+  {
+    src: "/img/speed-typing.png",
+    liveLink: "https://samirls.github.io/speedtyping/index.html",
+    githubLink: "https://github.com/samirls/speedtyping",
+    skills: [
+      { icon: <FaHtml5 />, color: "red.400" },
+      { icon: <FaCss3Alt />, color: "blue" },
+      { icon: <SiJavascript />, color: "yellow.300" },
+    ],
+  },
+  {
+    src: "/img/movie-blog.png",
+    liveLink: "notDeployed",
+    githubLink: "https://github.com/samirls/admin-Dashboard",
+    skills: [
+      { icon: <FaHtml5 />, color: "red.400" },
+      { icon: <FaCss3Alt />, color: "blue" },
+      { icon: <SiJavascript />, color: "yellow.300" },
+    ],
+  },
+  {
+    src: "/img/escala-glasgow.png",
+    liveLink: "https://samirls.github.io/glasgow/",
+    githubLink: "https://github.com/samirls/glasgow",
+    skills: [
+      { icon: <FaHtml5 />, color: "red.400" },
+      { icon: <FaCss3Alt />, color: "blue" },
+      { icon: <SiJavascript />, color: "yellow.300" },
+    ],
+  },
+  {
+    src: "/img/calculadora-trabalhista.png",
+    liveLink: "https://samirls.github.io/calculadoraclt/",
+    githubLink: "https://github.com/samirls/calculadoraclt",
+    skills: [
+      { icon: <FaHtml5 />, color: "red.400" },
+      { icon: <FaCss3Alt />, color: "blue" },
+      { icon: <SiJavascript />, color: "yellow.300" },
+    ],
+  },
+];
--- app/portfolio/carousel/carousel.js
+++ app/portfolio/carousel/carousel.js
@@ -1,93 +1,24 @@
 "use client";
 
-import { Box, Spinner } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
 import React, { useState } from "react";
 import {
   FaChevronLeft,
   FaChevronRight,
-  FaJava,
-  FaReact,
-  FaHtml5,
-  FaCss3Alt,
 } from "react-icons/fa";
-import { TbBrandNextjs } from "react-icons/tb";
-import {
-  BiLogoTypescript,
-  BiLogoSpringBoot,
-  BiLogoPostgresql,
-} from "react-icons/bi";
-import { SiMongodb, SiExpress, SiMongoose, SiJavascript } from "react-icons/si";
 import { BsCircle } from "react-icons/bs";
 import "animate.css";
 import SingleSlide from "./SingleSlide";
-import { useEffect } from "react";
-
-function PreloadImages({ images, onLoad }) {
-  const [loaded, setLoaded] = useState(false);
-
-  useEffect(() => {
-    const promises = images.map((src) => {
-      return new Promise((resolve, reject) => {
-        const img = new Image();
-        img.src = src;
-        img.onload = resolve;
-        img.onerror = reject;
-      });
-    });
-
-    Promise.all(promises)
-      .then(() => {
-        setLoaded(true);
-        if (onLoad) onLoad();
-      })
-      .catch((error) => console.error("Failed to preload images:", error));
-  }, [images, onLoad]);
-
-  return loaded ? null : (
-    <Box
-      display="flex"
-      flexDir="column"
-      justifyContent="center"
-      alignItems="center"
-      width="100%"
-      height="500px"
-    >
-      <Box>
-        <Spinner
-          thickness="4px"
-          speed="0.65s"
-          emptyColor="gray.200"
-          color="blue.500"
-          size="xl"
-        />
-      </Box>
-    </Box>
-  );
-}
+import { SlidesData } from "./SlidesData.js";
 
 function Carousel() {
-  const [currentPage, setCurrentPage] = useState(1);
-  const [imagesLoaded, setImagesLoaded] = useState(false);
+  const [currentPage, setCurrentPage] = useState(0);
   const [slideAnimation, setSlideAnimation] = useState(false);
 
-  const images = [
-    "/img/task-list-app.png",
-    "/img/drake-equation.png",
-    "/img/todo-family.png",
-    "/img/minha-clinica.png",
-    "/img/workout-buddy.png",
-    "/img/speed-typing.png",
-    "/img/movie-blog.png",
-    "/img/escala-glasgow.png",
-    "/img/calculadora-trabalhista.png",
-  ];
-
-  const handleImagesLoad = () => {
-    setImagesLoaded(true);
-  };
+  const images = SlidesData.map((slide) => slide.src);
 
   const goToPreviousPage = () => {
-    if (currentPage > 1) {
+    if (currentPage > 0) {
       setSlideAnimation(true);
       setTimeout(() => {
         setCurrentPage(currentPage - 1);
@@ -97,7 +28,7 @@ function Carousel() {
   };
 
   const goToNextPage = () => {
-    if (currentPage < 9) {
+    if (currentPage < images.length - 1) {
       setSlideAnimation(true);
       setTimeout(() => {
         setCurrentPage(currentPage + 1);
@@ -106,131 +37,38 @@ function Carousel() {
     }
   };
 
+  const renderSlide = (index) => {
+    const { src, liveLink, githubLink, skills } = SlidesData[index];
+    return (
+      <SingleSlide
+        key={index}
+        src={src}
+        slideAnimation={slideAnimation}
+        hrefLive={liveLink}
+        hrefGithub={githubLink}
+        skills={skills}
+      />
+    );
+  };
+
+  const renderNavigationCircles = () => {
+    return SlidesData.map((_, index) => (
+      <BsCircle
+        key={index}
+        cursor="pointer"
+        onClick={() => setCurrentPage(index)}
+        style={{
+          background: currentPage === index ? "black" : "transparent",
+          borderRadius: "50%",
+        }}
+      />
+    ));
+  };
+
   return (
     <Box display="flex" justifyContent="center">
-      <PreloadImages images={images} onLoad={handleImagesLoad} />
-      {imagesLoaded && (
         <Box className="animate__animated animate__fadeIn">
-          {currentPage === 1 && (
-            <SingleSlide
-              src={"/img/task-list-app.png"}
-              slideAnimation={slideAnimation}
-              hrefLive={"https://todo-next-14-kappa.vercel.app/"}
-              hrefGithub={"https://github.com/samirls/todo-next-14"}
-              skills={[
-                { icon: <TbBrandNextjs />, color: "gray.800" },
-                { icon: <BiLogoTypescript />, color: "blue.500" },
-                { icon: <BiLogoPostgresql />, color: "blue.700" },
-              ]}
-            />
-          )}
-          {currentPage === 2 && (
-            <SingleSlide
-              src={"/img/drake-equation.png"}
-              slideAnimation={slideAnimation}
-              hrefLive={"https://drake-equation-next-14.vercel.app/"}
-              hrefGithub={"https://github.com/samirls/drake-equation-next-14"}
-              skills={[
-                { icon: <TbBrandNextjs />, color: "gray.800" },
-                { icon: <BiLogoTypescript />, color: "blue.500" },
-                { icon: <BiLogoPostgresql />, color: "blue.700" },
-              ]}
-            />
-          )}
-          {currentPage === 3 && (
-            <SingleSlide
-              src={"/img/todo-family.png"}
-              slideAnimation={slideAnimation}
-              hrefLive={"notDeployed"}
-              hrefGithub={"https://github.com/samirls/todo-family-api"}
-              skills={[
-                { icon: <FaReact />, color: "blue.600" },
-                { icon: <FaJava />, color: "orange.700" },
-                { icon: <BiLogoSpringBoot />, color: "green.400" },
-                { icon: <BiLogoPostgresql />, color: "blue.700" },
-              ]}
-            />
-          )}
-          {currentPage === 4 && (
-            <SingleSlide
-              src={"/img/minha-clinica.png"}
-              slideAnimation={slideAnimation}
-              hrefLive={"notDeployed"}
-              hrefGithub={"https://github.com/samirls/med-track-api"}
-              skills={[
-                { icon: <TbBrandNextjs />, color: "gray.800" },
-                { icon: <FaJava />, color: "orange.700" },
-                { icon: <BiLogoSpringBoot />, color: "green.400" },
-                { icon: <BiLogoPostgresql />, color: "blue.700" },
-              ]}
-            />
-          )}
-          {currentPage === 5 && (
-            <SingleSlide
-              src={"/img/workout-buddy.png"}
-              slideAnimation={slideAnimation}
-              hrefLive={"notDeployed"}
-              hrefGithub={"https://github.com/samirls/workout-buddy-be"}
-              skills={[
-                { icon: <FaReact />, color: "blue.600" },
-                { icon: <SiExpress />, color: "black" },
-                { icon: <SiMongoose />, color: "red.700" },
-                { icon: <SiMongodb />, color: "green.600" },
-              ]}
-            />
-          )}
-          {currentPage === 6 && (
-            <SingleSlide
-              src={"/img/speed-typing.png"}
-              slideAnimation={slideAnimation}
-              hrefLive={"https://samirls.github.io/speedtyping/index.html"}
-              hrefGithub={"https://github.com/samirls/speedtyping"}
-              skills={[
-                { icon: <FaHtml5 />, color: "red.400" },
-                { icon: <FaCss3Alt />, color: "blue" },
-                { icon: <SiJavascript />, color: "yellow.300" },
-              ]}
-            />
-          )}
-          {currentPage === 7 && (
-            <SingleSlide
-              src={"/img/movie-blog.png"}
-              slideAnimation={slideAnimation}
-              hrefLive={"notDeployed"}
-              hrefGithub={"https://github.com/samirls/admin-Dashboard"}
-              skills={[
-                { icon: <FaHtml5 />, color: "red.400" },
-                { icon: <FaCss3Alt />, color: "blue" },
-                { icon: <SiJavascript />, color: "yellow.300" },
-              ]}
-            />
-          )}
-          {currentPage === 8 && (
-            <SingleSlide
-              src={"/img/escala-glasgow.png"}
-              slideAnimation={slideAnimation}
-              hrefLive={"https://samirls.github.io/glasgow/"}
-              hrefGithub={"https://github.com/samirls/glasgow"}
-              skills={[
-                { icon: <FaHtml5 />, color: "red.400" },
-                { icon: <FaCss3Alt />, color: "blue" },
-                { icon: <SiJavascript />, color: "yellow.300" },
-              ]}
-            />
-          )}
-          {currentPage === 9 && (
-            <SingleSlide
-              src={"/img/calculadora-trabalhista.png"}
-              slideAnimation={slideAnimation}
-              hrefLive={"https://samirls.github.io/calculadoraclt/"}
-              hrefGithub={"https://github.com/samirls/calculadoraclt"}
-              skills={[
-                { icon: <FaHtml5 />, color: "red.400" },
-                { icon: <FaCss3Alt />, color: "blue" },
-                { icon: <SiJavascript />, color: "yellow.300" },
-              ]}
-            />
-          )}
+          {renderSlide(currentPage)}
           <Box>
             <Box
               display="flex"
@@ -247,78 +85,7 @@ function Carousel() {
                 onClick={goToPreviousPage}
               />
               <Box display="flex" fontSize="0.8rem" gap="1">
-                <BsCircle
-                  cursor="pointer"
-                  onClick={() => setCurrentPage(1)}
-                  style={{
-                    background: currentPage === 1 ? "black" : "transparent",
-                    borderRadius: "50%",
-                  }}
-                />
-                <BsCircle
-                  cursor="pointer"
-                  onClick={() => setCurrentPage(2)}
-                  style={{
-                    background: currentPage === 2 ? "black" : "transparent",
-                    borderRadius: "50%",
-                  }}
-                />
-                <BsCircle
-                  cursor="pointer"
-                  onClick={() => setCurrentPage(3)}
-                  style={{
-                    background: currentPage === 3 ? "black" : "transparent",
-                    borderRadius: "50%",
-                  }}
-                />
-                <BsCircle
-                  cursor="pointer"
-                  onClick={() => setCurrentPage(4)}
-                  style={{
-                    background: currentPage === 4 ? "black" : "transparent",
-                    borderRadius: "50%",
-                  }}
-                />
-                <BsCircle
-                  cursor="pointer"
-                  onClick={() => setCurrentPage(5)}
-                  style={{
-                    background: currentPage === 5 ? "black" : "transparent",
-                    borderRadius: "50%",
-                  }}
-                />
-                <BsCircle
-                  cursor="pointer"
-                  onClick={() => setCurrentPage(6)}
-                  style={{
-                    background: currentPage === 6 ? "black" : "transparent",
-                    borderRadius: "50%",
-                  }}
-                />
-                <BsCircle
-                  cursor="pointer"
-                  onClick={() => setCurrentPage(7)}
-                  style={{
-                    background: currentPage === 7 ? "black" : "transparent",
-                    borderRadius: "50%",
-                  }}
-                />
-                <BsCircle
-                  cursor="pointer"
-                  onClick={() => setCurrentPage(8)}
-                  style={{
-                    background: currentPage === 8 ? "black" : "transparent",
-                    borderRadius: "50%",
-                  }}
-                />
-                <BsCircle
-                  cursor="pointer"
-                  onClick={() => setCurrentPage(9)}
-                  style={{
-                    background: currentPage === 9 ? "black" : "transparent",
-                    borderRadius: "50%",
-                  }}
-                />
+                {renderNavigationCircles()}
               </Box>
               <FaChevronRight
                 fontSize="2rem"
@@ -328,7 +95,6 @@ function Carousel() {
             </Box>
           </Box>
         </Box>
-      )}
     </Box>
   );
 }
